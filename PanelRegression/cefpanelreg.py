f9f42e7fd413a14cf9f59e5a4badaccfa08a81c2
Kan Punjathrapsin <kan.punjathrapsin@gmail.com>
2020-07-20 00:32:44 -0700

    add branch b1

--- PanelRegression/cefpanelreg.py
+++ PanelRegression/cefpanelreg.py
@@ -21,19 +21,21 @@ class CEFpanelreg:
     
     def __init__(self, file):
         self.filename = str(file)
+        print('Importing data...')
         data = pd.read_csv(self.filename, index_col = 0)
         data.columns = [x.lower() for x in data.columns]
-        data['date'] = pd.to_datetime(data['date']) 
-        print('Importing data...')
-        print(data)
+        data['date'] = pd.to_datetime(data['date'])
+        print('Import success')
         self.data = data
+        self.sumstat = {}
         
     def __call__(self, collist):
         self.__Checkcol(collist, self.data)
     
     def result(self,
-               start_datetime = datetime(2005, 1, 1),
-               end_datetime = datetime(2005, 1, 1),
+               start_datetime = datetime(2020, 1, 1),
+               end_datetime = datetime(2000, 1, 1),
+               y = ['cd'],
                var_pit = [],
                var_norm = [],
                fix = [],                
@@ -46,71 +48,68 @@ class CEFpanelreg:
         # date variables
         self.data['year'] = pd.DatetimeIndex(self.data['date']).year
         self.data['month'] = pd.DatetimeIndex(self.data['date']).month
-        self.data['yrmo'] = self.data['year']*12 + self.data['month']
         self.data['inceptiondate'] = pd.to_datetime(self.data['inceptiondate'])
         self.data = self.data.drop_duplicates(subset=['ticker','date'],keep='last')
         
         # check valid lags, drop later before regression
         self.__CheckValidLag()
         
+        # change in discount
+        self.data['lpd'] = self.data[['ticker','pd']].groupby('ticker').shift(1)
+        self.data['cd'] = self.data['pd'] - self.data['lpd']
+        
         # age
         self.data['age'] = (self.data['date']-self.data['inceptiondate']).dt.days
         
-        # discount variables
-        #self.data['pd'] = 
-        #df['lpd'] = df[['ticker','pd']].groupby('ticker').shift(1)
+        # column reference
+        c = len(self.data.columns)
         
         # Point-in-time variables
         for var in var_pit:
             variable,lag = var[0],var[1]
-            #for absolte day lag
-            #temp = self.data[['ticker','date',variable]].copy()
-            #temp['date'] = temp['date']-timedelta(days=2)
-            #temp = temp.rename(columns = {variable:variable+'-'+str(lag)})
-            #self.data = pd.merge(self.data, temp, how='left', on=['ticker','date'])
-            self.data[variable+'-'+str(lag)] = self.data.groupby(['ticker'])[variable].shift(lag)
+            self.data[variable+'_'+str(lag)] = self.data.groupby(['ticker'])[variable].shift(lag)
         
         # Normalized variables
         def decor(func):
-            def wrapper(x,y,z):
-                print("Normalizing by "+func.__name__+"...")
-                return func(x,y,z)
+            def wrapper(x,y,z,var):
+                print("Regressing on "+str(var)+" using "+str(z)+"-day "+func.__name__+" from lag "+str(y)+"...")
+                return func(x,y,z,var)
             return wrapper
         @decor
-        def mean(df,lag,length):
-            df[variable+'-'+str(lag)+'-'+f+str(length)] = df.groupby('ticker')[variable].shift(lag).rolling(length).mean()
+        def mean(df,lag,length,var):
+            df[variable+'_'+str(lag)+'_'+f+str(length)] = df.groupby('ticker')[variable].shift(lag).rolling(length).mean()
         @decor
-        def std(df,lag,length):
-            df[variable+'-'+str(lag)+'-'+f+str(length)] = df.groupby('ticker')[variable].shift(lag).rolling(length).std()
+        def std(df,lag,length,var):
+            df[variable+'_'+str(lag)+'_'+f+str(length)] = df.groupby('ticker')[variable].shift(lag).rolling(length).std()
             
         func_dict = {'mean':mean,
                      'std':std}
         
         for var in var_norm:
             variable,lag,length,f = var[0],var[1],var[2],var[3]
-            func_dict[f](self.data,lag,length)
+            func_dict[f](self.data,lag,length,variable)
         
-            
-        return self.data
+        # fit regression and return results
+        result = self.__fitreg(self.data, start_datetime, end_datetime, y, var_pit, var_norm, fix, cluster, c)
         
+        # extract .nobs, .rsquared, .params, .tstats
+        self.sumstat['R2'] = round(result.rsquared,4)
+        self.sumstat['N'] = result.nobs
+        self.sumstat['Coefficient'] = round(result.params,4)
+        self.sumstat['t-stat'] = round(result.tstats,4)
         
+        return result
+
+    def summary(self):
+        
+        print(pd.concat([self.sumstat['Coefficient'],self.sumstat['t-stat']], axis = 1))
+        print("R2 = {}".format(self.sumstat['R2']))
+        print("N = {}".format(self.sumstat['N']))
+
         #temp = df1.sort_values('date').groupby(['ticker','year','month']).tail(1) #use last observation of the month
         #temp = temp[['ticker','year','month','volume']]
         #temp = temp.rename(columns = {'volume':'volume'+'-'+str(lag)+str(unit)})
         #df1 = pd.merge(df, temp, how='left', on=['ticker','year','month'])
-
-
-        
-            
-        
-
-        
-        
-        #var = {i.lower() for i in var}
-        
-        
-        
-        
         
         
     def __Checkcol(self, collist, df):
@@ -161,5 +160,58 @@ class CEFpanelreg:
         #self.data['valid'] = [d<limit for d in self.data['dif']]
         self.data.drop(['dif'], axis=1, inplace=True)
         
+    def __fitreg(self,
+                 dt,
+                 start_datetime,
+                 end_datetime,
+                 y,
+                 var_pit,
+                 var_norm,
+                 fix,
+                 cluster,
+                 c
+                 ):
         
+        # filter dates
+        dt = dt.loc[(dt['date']>=start_datetime) & (dt['date']<=end_datetime)]
         
+        # filter columns
+        dt = dt[y + ['year','ticker'] + [col for col in dt.columns[c:]] + fix]
+        
+        # choose x
+        x = '+'.join(dt.columns[3:])
+        
+        #print("Start filling NAs...")
+        #dt = dt.fillna(dt.groupby('ticker').transform('mean'))
+        #dt = dt.fillna(dt.transform('mean'))
+        dt = dt.dropna()
+        #print("Filling NAs done.")
+        dt = dt.set_index(['ticker','year'])
+          
+        if len(fix) == 0 and len(cluster) == 0:
+            mod = PanelOLS.from_formula(y[0] + '~1+' + x, data = dt)
+            fit1 = mod.fit(cov_type = 'clustered', cluster_time = False, cluster_entity = False)
+            return fit1
+        
+        if len(fix) == 1:
+            mod = PanelOLS.from_formula(y[0] + '~1+' + x + '+' + fix[0], data = dt)
+            if len(cluster) == 0:
+                fit1 = mod.fit(cov_type = 'clustered', cluster_time = False, cluster_entity = False)
+                return fit1
+            elif cluster == ['year']:
+                fit1 = mod.fit(cov_type = 'clustered', cluster_time = True, cluster_entity = False)
+                return fit1
+            elif cluster == ['ticker']:
+                fit1 = mod.fit(cov_type = 'clustered', cluster_time = False, cluster_entity = True)
+                return fit1
+            elif cluster == ['year','ticker'] or cluster == ['ticker','year']:
+                fit1 = mod.fit(cov_type = 'clustered', cluster_time = True, cluster_entity = True)
+                return fit1
+            else:
+                raise KeyError("Please choose either year or ticker, or both.")
+            
+        if len(fix) > 1:
+            raise KeyError("You have {} fixed effects! Please pick one.".format(len(fix)))
+        
+
+    
--- PanelRegression/panelreg.py
+++ PanelRegression/panelreg.py
@@ -6,7 +6,6 @@ Created on Sat Jul 18 14:01:43 2020
 @author: kanp
 """
 
-from datetime import datetime
 from cefpanelreg import CEFpanelreg
 
 """
@@ -15,43 +14,34 @@ STEP1: Input file name
 
 filename = 'US_data.csv'
 
-cef = CEFpanelreg(filename)
-
 """
 STEP2: Input parameters for regression
 - start datetime, end datetime in 'YYYY-MM-DD'
-- Point-in-time independent variables in [variable, lag] e.g. ['volume',1]; unit in day
-- Normalized independent variables in [variable, lag, length, func] e.g. [cd,1,3,mean]; unit in day
+- y: what you want to predict; default is 'cd'
+- var_pit: Point-in-time independent variables in [variable, lag]; unit in day
+    e.g. ['volume',1] >> regress on lag1 of volume
+- var_norm: Normalized independent variables in [variable, lag, length, func]; unit in day
+    e.g. [cd,1,3,mean] >> regress on 3-day mean from lag1 of cd
+- fix: Fixed effects; choose one from ['assetclasslevel1','assetclasslevel2','assetclasslevel3']
+- Cluster: Covariance clustering; choose from ['year','ticker']
 """
 
-#results = 
-
+cef = CEFpanelreg(filename)
 cef.result(
-        start_datetime = '1999-02-01',
-        end_datetime = '2020-1-1',
-        var_pit = [['volume',1]],
-        var_norm = [['volume',1,3,'mean'],['pd',20,20,'std']],
-        fix = [],
-        cluster = []
+        start_datetime = '2019-02-01',
+        end_datetime = '2020-4-1',
+        y = ['cd'],
+        var_pit = [['cd',1]],
+        var_norm = [['volume',1,10,'std'],['cd',1,10,'mean'],['cd',1,10,'std']],
+        fix = ['assetclasslevel3'],
+        cluster = ['year','ticker']
         )
+cef.summary()
 
 
+"""
 
-
-
-
-
-
-
-
-#for choosing specific columns
-cef(['ticker','volume','s'])
+# view specific columns
 cef(['ticker','volume'])
 
-
-
-
-#pd.read_csv('US_data.csv')
-
-print('sss')
-print(data)
+"""
